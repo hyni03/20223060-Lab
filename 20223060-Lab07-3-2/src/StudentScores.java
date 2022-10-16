a6156baf4bd6486e52ec422ef295f6e339192623
Eunji Kim <47711027+hyni03@users.noreply.github.com>
2022-10-17 01:40:45 +0900

    Add files via upload

---
+++ 20223060-Lab07-3-2/src/StudentScores.java
@@ -0,0 +1,44 @@
+
+public class StudentScores {
+	private final int MAX_STUDENTS = 100;
+	private Student[] students;
+	private int numStudents;
+
+	public StudentScores() {
+		students = new Student[MAX_STUDENTS];
+		numStudents = 0;
+	}
+
+	public void add(String name, int score) {
+		if (numStudents >= MAX_STUDENTS)
+			return; // not enough space to add new student score
+		students[numStudents] = new Student(name, score);
+		numStudents++;
+	}
+
+	public String getHighest() {
+		if (numStudents == 0)
+			return null;
+
+		int highest = 0;
+
+		for (int i = 1; i < numStudents; i++)
+			if (students[i].Score() > students[highest].Score())
+				highest = i;
+
+		return students[highest].Name();
+	}
+
+	public String getLowest() {
+		if (numStudents == 0)
+			return null;
+
+		int lowest = 0;
+
+		for (int i = 1; i < numStudents; i++)
+			if (students[i].Score() < students[lowest].Score())
+				lowest = i;
+
+		return students[lowest].Name();
+	}
+}
